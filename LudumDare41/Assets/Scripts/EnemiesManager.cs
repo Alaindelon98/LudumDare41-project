f6ace82eb3cb57095acc2195d10feb41304fab26
annamde <annadiaz553@gmail.com>
2018-04-21 17:20:11 +0100

    enemigos y manager enemigos

---
+++ LudumDare41/Assets/Scripts/EnemiesManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemiesManager : MonoBehaviour {
+
+	//public GameObject player;
+	public float distanceMax;
+	public List<enemyScript> enemies;
+	private Vector3 distanceVec;
+	private float distance;
+
+
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		ActivateEnemies ();
+	}
+
+	void ActivateEnemies()
+	{
+		foreach (enemyScript item in enemies) {
+			distanceVec = GameManagerScript.player.transform.position - item.gameObject.transform.position;
+			//distanceVec = player.transform.position - item.gameObject.transform.position;
+			distance = distanceVec.magnitude;
+			if (distance <= distanceMax)
+			{
+				item.enabled = true;
+			}
+			else if (distance >= distanceMax)
+			{
+				item.enabled = false;
+			}
+		}
+
+	}
+}
---
+++ LudumDare41/Assets/Scripts/enemyScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyScript : MonoBehaviour {
+
+
+	public float speed;
+	private int direction=1;
+
+	// Use this for initialization
+	void Start () {
+		this.enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		Mov ();
+	}
+
+	private void OnTriggerEnter2D(Collider2D col)
+	{
+		if (col.gameObject.tag == "Right")
+		{
+			direction = -Mathf.Abs(direction);
+		}
+		if (col.gameObject.tag == "Left")
+		{
+			direction = Mathf.Abs(direction);
+		}
+	}
+
+	private void Mov()
+	{
+		transform.position += new Vector3 (direction*speed*Time.deltaTime,0,0);
+		//saltito 
+	}
+}
