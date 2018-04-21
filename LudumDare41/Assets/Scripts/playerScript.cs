f5c62bfa35894fbbc919d51f1cf4516d6d674f88
annamde <annadiaz553@gmail.com>
2018-04-21 14:19:37 +0100

    carpetas y scirpt de player

---
+++ LudumDare41/Assets/Scripts/playerScript.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerScript : MonoBehaviour {
+
+	public float maxSpeed, initialSpeed;
+
+	private float speed;
+	private Rigidbody2D myRb;
+
+
+	// Use this for initialization
+	void Start () 
+	{
+		myRb = GetComponent<Rigidbody2D> ();
+		speed = initialSpeed;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		Mov ();
+
+		if (Input.GetKeyDown("space"))
+		{
+			print ("espaciuuu");
+		}
+
+	}
+	void Mov()
+	{
+		myRb.velocity = new Vector2 (speed, myRb.velocity.y);
+	}
+		
+	void Jump()
+	{
+
+	}
+
+	void Sprint()
+	{
+
+	}
+}
