235b4f9e95aaefb89b21cbe64335ba28ec95939e
adrinuga <38584756+adrinuga@users.noreply.github.com>
2018-04-22 12:30:12 +0200

    Playerpref

---
+++ LudumDare41/Assets/Scripts/ColScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColScript : MonoBehaviour {
+
+    public enum ColType { Legs, Head, Body };
+    public ColType theseColType;
+    private playerScript myPlayerScript;
+
+	// Use this for initialization
+	void Start ()
+    {
+        myPlayerScript = gameObject.GetComponentInParent<playerScript>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+    void OnCollisionEnter2D()
+    {
+
+    }
+}
