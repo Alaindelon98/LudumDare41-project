730c46f014415ea510783935d183ad97f11ac580
ssngrss <38585475+ssngrss@users.noreply.github.com>
2018-04-23 03:37:02 +0200

    menuscene

---
+++ LudumDare41/Assets/menuScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class menuScript : MonoBehaviour {
+
+	public void loadGame(){
+		SceneManager.LoadScene (1);
+	}
+
+	public void quit(){
+		Application.Quit ();
+	}
+}
